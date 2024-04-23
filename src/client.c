3ea668f0adb505c4bbaf99eabdb1903a66755351
Tonnam Somchanakij <tsomchan@z1t3c2.42bangkok.com>
2024-04-23 15:35:11 +0700

    use bitwise operators

--- src/client.c
+++ src/client.c
@@ -3,8 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsomchan <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*   By: tsomchan <marvin@42.fr>                    +#+  +:+       +#+        */ /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 12:57:37 by tsomchan          #+#    #+#             */
 /*   Updated: 2024/04/18 12:57:40 by tsomchan         ###   ########.fr       */
 /*                                                                            */
@@ -12,6 +11,47 @@
 
 #include "../inc/minitalk.h"
 
+void	send_char(char c, int pid)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (c & (0b10000000 >> i))
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		i++;
+		usleep(500);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		pid = ft_atoi((const char *)argv[1]);
+		if (pid <= 0)
+		{
+			write(1, "Invalid PID!\n", 13);
+			return (0);
+		}
+		while (argv[2][i])
+			send_char((int)argv[2][i++], pid);
+	}
+	else
+		write(1, "Usage: ./client [PID] [STRING_TO_PASS]\n", 40);
+	return (0);
+}
+
+//printf("(int)argv[2][i] = \'%c\' %d\n", (int)argv[2][i], (int)argv[2][i]);
+
+/*
 void	send_signal(int bit, int pid)
 {
 	if (bit == 0)
@@ -38,12 +78,12 @@ void	convert(int c, int pid)
 {
 	t_byte	byte;
 
-	//*(unsigned char *)&byte = (unsigned char)c;
 	*(unsigned char *)&byte = (unsigned char)c;
-	//printf("c = \'%c\' %d\n", c, c);
-	//printf("byte = \'%c\' %d %d%d%d%d%d%d%d%d\n",
-		//*(unsigned char *)&byte, *(unsigned char *)&byte,
-		//byte.b1, byte.b2, byte.b3, byte.b4, byte.b5, byte.b6, byte.b7, byte.b8);
+	*(unsigned char *)&byte = (unsigned char)c;
+	printf("c = \'%c\' %d\n", c, c);
+	printf("byte = \'%c\' %d %d%d%d%d%d%d%d%d\n",
+		*(unsigned char *)&byte, *(unsigned char *)&byte,
+		byte.b1, byte.b2, byte.b3, byte.b4, byte.b5, byte.b6, byte.b7, byte.b8);
 	send_signal(byte.b1, pid);
 	send_signal(byte.b2, pid);
 	send_signal(byte.b3, pid);
@@ -53,29 +93,4 @@ void	convert(int c, int pid)
 	send_signal(byte.b7, pid);
 	send_signal(byte.b8, pid);
 }
-
-int	main(int argc, char **argv)
-{
-	int	pid;
-	int	i;
-
-	i = 0;
-	if (argc == 3)
-	{
-		pid = ft_atoi((const char *)argv[1]);
-		if (pid == 0 || pid < 0)
-		{
-			write(1, "Invalid PID!\n", 13);
-			return (0);
-		}
-		while (argv[2][i])
-		{
-			//printf("(int)argv[2][i] = \'%c\' %d\n", (int)argv[2][i], (int)argv[2][i]);
-			convert((int)argv[2][i], pid);
-			i++;
-		}
-	}
-	else
-		write(1, "Usage: ./client [PID] [STRING_TO_PASS]\n", 40);
-	return (0);
-}
+*/
--- src/server.c
+++ src/server.c
@@ -11,16 +11,65 @@
 
 #include "../inc/minitalk.h"
 
+void	handler(int sig)
+{
+	static char	c = 0b00000000;
+	static int	i = 0;
+
+	if (sig == SIGUSR2)
+		c |= (0b10000000 >> i);
+	i++;
+	if (i == 8)
+	{
+		write(1, &c, 1);
+		c = 0b00000000;
+		i = 0;
+	}
+}
+
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = &handler;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	write(1, "PID: ", 6);
+	ft_putnbr(getpid());
+	write(1, "\n", 1);
+	while (1)
+		pause();
+	return (0);
+}
+
+/*
+	if (!i)
+		i = 1;
+	if (sig == SIGUSR1)
+	{
+		set_bit(0, &i, &byte);
+		i++;
+	}
+	if (sig == SIGUSR2)
+	{
+		set_bit(1, &i, &byte);
+		i++;
+	}
+*/
+
+/*
 t_byte		byte;
 
 void	set_bit(int bit, int *i, t_byte *byte)
 {
-	//printf("i = %d ", *i);
-	//printf("sig = %d ", bit);
-	//if (bit == 0)
-	//	printf("SIGUSR1\n", SIGUSR1);
-	//if (bit == 1)
-	//	printf("SIGUSR2\n", SIGUSR2);
+	printf("i = %d ", *i);
+	printf("sig = %d ", bit);
+	if (bit == 0)
+		printf("SIGUSR1\n", SIGUSR1);
+	if (bit == 1)
+		printf("SIGUSR2\n", SIGUSR2);
 	if (*i == 1)
 		byte->b1 = bit;
 	if (*i == 2)
@@ -39,49 +88,16 @@ void	set_bit(int bit, int *i, t_byte *byte)
 	{
 		byte->b8 = bit;
 		write(1, &*(unsigned char *)byte, 1);
-		//write(1, "\n", 1);
+		write(1, "\n", 1);
 		*i = 0;
-		//printf("byte->= \'%c\' %d %d%d%d%d%d%d%d%d\n",
-		//	*(unsigned char *)byte, *(unsigned char *)byte,
-		//	byte->b1, byte->b2, byte->b3, byte->b4,
-		//	byte->b5, byte->b6, byte->b7, byte->b8);
+		printf("byte->= \'%c\' %d %d%d%d%d%d%d%d%d\n",
+			*(unsigned char *)byte, *(unsigned char *)byte,
+			byte->b1, byte->b2, byte->b3, byte->b4,
+			byte->b5, byte->b6, byte->b7, byte->b8);
 	}
-	//printf("byte->= \'%c\' %d %d%d%d%d%d%d%d%d\n",
-	//	*(unsigned char *)byte, *(unsigned char *)byte,
-	//	byte->b1, byte->b2, byte->b3, byte->b4,
-	//	byte->b5, byte->b6, byte->b7, byte->b8);
+	printf("byte->= \'%c\' %d %d%d%d%d%d%d%d%d\n",
+		*(unsigned char *)byte, *(unsigned char *)byte,
+		byte->b1, byte->b2, byte->b3, byte->b4,
+		byte->b5, byte->b6, byte->b7, byte->b8);
 }
-
-void	handler(int sig)
-{
-	static int	i;
-
-	if (!i)
-		i = 1;
-	if (sig == SIGUSR1)
-	{
-		//set_bit(0, &i, &byte);
-		i++;
-	}
-	if (sig == SIGUSR2)
-	{
-		//set_bit(1, &i, &byte);
-		i++;
-	}
-}
-
-int	main(void)
-{
-	struct sigaction	sa;
-
-	sa.sa_handler = &handler;
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	write(1, "PID: ", 6);
-	ft_putnbr(getpid());
-	write(1, "\n", 1);
-	while (1)
-		pause();
-	return (0);
-}
+*/
